7661489d1d098e05f5136b8b4c224045c2fc37ce
CleanAsis <ivan.one.only@gmail.com>
2014-04-23 14:14:32 -0700

    Layout for the picker window is pretty complete

--- SenchaProServices/colorpicker/Window.js
+++ SenchaProServices/colorpicker/Window.js
@@ -5,8 +5,17 @@ Ext.define('SenchaProServices.colorpicker.Window', {
     extend     : 'Ext.container.Container',
     alias      : 'widget.sps_colorpickerwindow',
 
+    requires: [
+        'Ext.layout.container.Border',
+        'Ext.layout.container.HBox'
+    ],
+
+    sliderWidth : 30, // width of every slider
+    sliderPad   : 5,  // padding between the sliders AND HEX/R/G/B fields
+
     constructor: function(cfg) {
         var me = this;
+
         Ext.applyIf(cfg, {
             cls      : 'sps-colorpicker-window',
             width    : 500,
@@ -15,173 +24,254 @@ Ext.define('SenchaProServices.colorpicker.Window', {
             floating : true,
             layout   : 'border',
             items: [
-                // TOP REGION
+                // Map, Sliders, Fields, and Preview (center region)
                 {
-                    xtype: 'container',
-                    region: 'center',
-                    layout: {
-                        type: 'hbox',
-                        align: 'stretch'
+                    xtype  : 'container',
+                    region : 'center',
+                    layout : {
+                        type  : 'hbox',
+                        align : 'stretch'
                     },
                     items: [
-                        // "MAP" and HEX/R/G/B FIELDS
-                        {
-                            xtype: 'container',
-                            flex: 1,
-                            layout: {
-                                type: 'vbox',
-                                align: 'stretch'
-                            },
-                            margin: '0 2 0 0',
-                            items: [
-                                // "MAP"
-                                {
-                                    xtype: 'component',
-                                    style: 'background: tomato;',
-                                    html: 'Map',
-                                    flex: 1,
-                                    margin: '0 0 5 0'
-                                },
-                                // HEX/R/G/B FIELDS
-                                {
-                                    xtype: 'container',
-                                    defaults: {
-                                        style: 'display: inline-table; margin-right: 2px;'
-                                    },
-                                    items: [
-                                        {
-                                            xtype: 'field',
-                                            fieldLabel: 'HEX',
-                                            labelAlign: 'top'
-                                        },
-                                        {
-                                            xtype: 'field',
-                                            fieldLabel: 'R',
-                                            labelAlign: 'top'
-                                        },
-                                        {
-                                            xtype: 'field',
-                                            fieldLabel: 'G',
-                                            labelAlign: 'top'
-                                        },
-                                        {
-                                            xtype: 'field',
-                                            fieldLabel: 'B',
-                                            labelAlign: 'top'
-                                        }
-                                    ]
-                                }
-                            ]
-                        },
-                        // SLIDER and H FIELD
-                        {
-                            xtype: 'container',
-                            width: 20,
-                            layout: {
-                                type: 'vbox',
-                                align: 'stretch'
-                            },
-                            margin: '0 2 0 2',
-                            items: [
-                                {
-                                    xtype: 'component',
-                                    style: 'background: cornflowerblue;',
-                                    flex: 1,
-                                    margin: '0 0 5 0'
-                                },
-                                {
-                                    xtype: 'field',
-                                    fieldLabel: 'H',
-                                    labelAlign: 'top'
-                                }
-                            ]
-                        },
-                        // SLIDER and S FIELD 
-                        {
-                            xtype: 'container',
-                            width: 20,
-                            layout: {
-                                type: 'vbox',
-                                align: 'stretch'
-                            },
-                            margin: '0 2 0 2',
-                            items: [
-                                {
-                                    xtype: 'component',
-                                    style: 'background: cornflowerblue;',
-                                    flex: 1,
-                                    margin: '0 0 5 0'
-                                },
-                                {
-                                    xtype: 'field',
-                                    fieldLabel: 'S',
-                                    labelAlign: 'top'
-                                }
-                            ]
-                        },
-                        // SLIDER and V FIELD 
-                        {
-                            xtype: 'container',
-                            width: 20,
-                            layout: {
-                                type: 'vbox',
-                                align: 'stretch'
-                            },
-                            margin: '0 2 0 2',
-                            items: [
-                                {
-                                    xtype: 'component',
-                                    style: 'background: cornflowerblue;',
-                                    flex: 1,
-                                    margin: '0 0 5 0'
-                                },
-                                {
-                                    xtype: 'field',
-                                    fieldLabel: 'V',
-                                    labelAlign: 'top'
-                                }
-                            ]
-                        },
-                        // SLIDER and A FIELD 
-                        {
-                            xtype: 'container',
-                            width: 20,
-                            layout: {
-                                type: 'vbox',
-                                align: 'stretch'
-                            },
-                            margin: '0 2 0 2',
-                            items: [
-                                {
-                                    xtype: 'component',
-                                    style: 'background: cornflowerblue;',
-                                    flex: 1,
-                                    margin: '0 0 5 0'
-                                },
-                                {
-                                    xtype: 'field',
-                                    fieldLabel: 'A',
-                                    labelAlign: 'top'
-                                }
-                            ]
-                        },
-                        // PREVIEW
-                        {
-                            xtype: 'component',
-                            style: 'background: cornflowerblue;',
-                            width: 100
-                        }
+                        me.getMapAndHexRGBFields(),
+                        me.getSliderAndHField(),
+                        me.getSliderAndSField(),
+                        me.getSliderAndVField(),
+                        me.getSliderAndAField(),
+                        me.getPreviewAndButtons()
                     ]
                 },
-                // PALLETTE
+                // Pallette and "Save" button (bottom region)
                 {
-                    xtype: 'container',
-                    region: 'south',
-                    style: 'background: orange;',
-                    html: 'Pallette'
+                    xtype  : 'container',
+                    region : 'south',
+                    layout: {
+                        type  : 'hbox',
+                        align : 'stretch'
+                    },
+                    items: [
+                        {
+                            xtype  : 'component',
+                            style  : 'background: orange;',
+                            html   : 'Pallette',
+                            flex   : 1,
+                            margin : '0 10 0 0'
+                        },
+                        {
+                            xtype : 'button',
+                            text  : 'Save',
+                            width : 70
+                        }
+                    ]
                 }
             ]
         });
+
         me.callParent(arguments);
+    },
+
+    // Splits up view declaration for readability
+    // "Map" and HEX/R/G/B fields
+    getMapAndHexRGBFields: function() {
+        var me = this;
+        return {
+            xtype  : 'container',
+            flex   : 1,
+            layout : {
+                type  : 'vbox',
+                align : 'stretch'
+            },
+            margin : '0 10 0 0',
+            items  : [
+                // "MAP"
+                {
+                    xtype  : 'component',
+                    style  : 'background: tomato;',
+                    html   : 'Map',
+                    flex   : 1
+                },
+                // HEX/R/G/B FIELDS
+                {
+                    xtype    : 'container',
+                    defaults : {
+                        style          : 'display: inline-table; margin-right: ' + me.sliderPad + 'px;',
+                        labelSeparator : ''
+                    },
+                    items: [
+                        {
+                            xtype      : 'field',
+                            fieldLabel : 'HEX',
+                            labelAlign : 'top'
+                        },
+                        {
+                            xtype      : 'field',
+                            fieldLabel : 'R',
+                            labelAlign : 'top'
+                        },
+                        {
+                            xtype      : 'field',
+                            fieldLabel : 'G',
+                            labelAlign : 'top'
+                        },
+                        {
+                            xtype      : 'field',
+                            fieldLabel : 'B',
+                            labelAlign : 'top'
+                        }
+                    ]
+                }
+            ]
+        };
+    },
+
+    // Splits up view declaration for readability
+    // Slider and H field 
+    getSliderAndHField: function() {
+        var me = this;
+        return {
+            xtype  : 'container',
+            width  : me.sliderWidth,
+            layout : {
+                type  : 'vbox',
+                align : 'stretch'
+            },
+            items  : [
+                {
+                    xtype  : 'component',
+                    style  : 'background: cornflowerblue;',
+                    flex   : 1
+                },
+                {
+                    xtype          : 'field',
+                    fieldLabel     : 'H',
+                    labelAlign     : 'top',
+                    labelSeparator : ''
+                }
+            ]
+        };
+    },
+
+    // Splits up view declaration for readability
+    // Slider and S field 
+    getSliderAndSField: function() {
+        var me = this;
+        return {
+            xtype  : 'container',
+            width  : me.sliderWidth,
+            layout : {
+                type  : 'vbox',
+                align : 'stretch'
+            },
+            margin: {
+                right  : me.sliderPad,
+                left   : me.sliderPad
+            },
+            items  : [
+                {
+                    xtype  : 'component',
+                    style  : 'background: cornflowerblue;',
+                    flex   : 1
+                },
+                {
+                    xtype          : 'field',
+                    fieldLabel     : 'S',
+                    labelAlign     : 'top',
+                    labelSeparator : ''
+                }
+            ]
+        };
+    },
+
+    // Splits up view declaration for readability
+    // Slider and V field 
+    getSliderAndVField: function() {
+        var me = this;
+        return {
+            xtype  : 'container',
+            width  : me.sliderWidth,
+            layout : {
+                type  : 'vbox',
+                align : 'stretch'
+            },
+            items  : [
+                {
+                    xtype  : 'component',
+                    style  : 'background: cornflowerblue;',
+                    flex   : 1
+                },
+                {
+                    xtype          : 'field',
+                    fieldLabel     : 'V',
+                    labelAlign     : 'top',
+                    labelSeparator : ''
+                }
+            ]
+        };
+    },
+
+    // Splits up view declaration for readability
+    // Slider and A field 
+    getSliderAndAField: function() {
+        var me = this;
+        return {
+            xtype  : 'container',
+            width  : me.sliderWidth,
+            layout : {
+                type  : 'vbox',
+                align : 'stretch'
+            },
+            margin: {
+                left   : me.sliderPad
+            },
+            items  : [
+                {
+                    xtype  : 'component',
+                    style  : 'background: cornflowerblue;',
+                    flex   : 1
+                },
+                {
+                    xtype          : 'field',
+                    fieldLabel     : 'A',
+                    labelAlign     : 'top',
+                    labelSeparator : ''
+                }
+            ]
+        };
+    },
+
+    // Splits up view declaration for readability
+    // Preview squares and OK and Cancel buttons
+    getPreviewAndButtons: function() {
+        return {
+            xtype  : 'container',
+            width  : 70,
+            margin : '0 0 0 10',
+            layout : {
+                type  : 'vbox',
+                align : 'stretch'
+            },
+            items: [
+                {
+                    xtype  : 'component',
+                    height : 60,
+                    style  : 'background: sandybrown'
+                },
+                {
+                    xtype  : 'component',
+                    height : 60,
+                    style  : 'background: saddlebrown'
+                },
+                {
+                    xtype  : 'button',
+                    text   : 'OK',
+                    margin : '10 0 0 0'
+                },
+                {
+                    xtype  : 'button',
+                    text   : 'Cancel',
+                    margin : '10 0 0 0'
+                }
+            ]
+        };
     }
 });
